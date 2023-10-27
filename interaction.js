2b346f08d871d0fac3546d58b08c41336031e0fa
michelle-viet <michelleviet01@gmail.com>
2023-10-26 18:38:09 -0700

    FINALLY got the cards working

--- interaction.js
+++ interaction.js
@@ -19,6 +19,16 @@ function resetLogo(element) {
     logo.src = 'images/logo/logo-core-black.svg';
 }
 
+// const logo = document.getElementById('logo');
+// const logoVariations = ['https://picsum.photos/500/300/?image=17', 'https://picsum.photos/500/300/?image=18', 'https://picsum.photos/500/300/?image=19', 'https://picsum.photos/500/300/?image=16', 'https://picsum.photos/500/300/?image=15', 'https://picsum.photos/500/300/?image=14', 'https://picsum.photos/500/300/?image=11', 'https://picsum.photos/500/300/?image=12'];
+// let currentIndex = 0;
+
+// logo.addEventListener('mouseover', () => {
+//     currentIndex = (currentIndex + 1) % logoVariations.length;
+//     logo.src = logoVariations[currentIndex];
+// });
+
+
 
 
 // ███    ██  █████  ██    ██     ███    ███ ███████ ███    ██ ██    ██ 
