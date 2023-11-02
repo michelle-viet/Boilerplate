00a8668477712d563ce4a47911cbdd05f9212c4e
michelle-viet <michelleviet01@gmail.com>
2023-11-02 11:37:08 -0700

    FINALLY FIGURED OUT THE FILTER

--- filter/README.md
+++
@@ -1,4 +0,0 @@
-# Create Filterable DIV Elements
-
-A Pen created on CodePen.io. Original URL: [https://codepen.io/jamesneufeld/pen/GRzZwyy](https://codepen.io/jamesneufeld/pen/GRzZwyy).
-
--- play-script.js
+++ play-script.js
@@ -36,8 +36,8 @@ function w3RemoveClass(element, name) {
 }
 
 // Add active class to the current control button (highlight it)
-var btnContainer = document.getElementById("myBtnContainer");
-var btns = btnContainer.getElementsByClassName("btn");
+var btnContainer = document.getElementById("play-filter");
+var btns = btnContainer.getElementsByClassName("filter-btn");
 for (var i = 0; i < btns.length; i++) {
   btns[i].addEventListener("click", function () {
     var current = document.getElementsByClassName("active");
